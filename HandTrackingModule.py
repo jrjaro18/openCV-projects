3808d806a47c49464dd5fb43148d780c3e24a6fe
jrjaro18 <rohanjaiswal1500@gmail.com>
2023-09-23 00:56:39 +0530

    Hand Tracking Module Added

---
+++ HandTrackingModule.py
@@ -0,0 +1,60 @@
+import cv2
+import mediapipe as mp
+import time
+
+class HandDetector:
+    def __init__(self, mode=False, maxHands=2,modelComplx = 1, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.modelComplx = modelComplx
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplx, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+
+
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self,img,handNo=0,draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx , cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),7,(255,0,255),cv2.FILLED)
+        return lmList
+
+def main():
+    ptime = 0
+    ctime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        liList = detector.findPosition(img)
+        if len(liList)!=0:
+            print(liList[4])
+        ctime = time.time()
+        fps = 1 / (ctime - ptime)
+        ptime = time.time()
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, cv2.COLOR_Luv2RGB, 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
